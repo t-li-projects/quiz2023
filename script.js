f7e2245f69eb8b34ae844205690717e1461cebad
t-li-projects <130779164+t-li-projects@users.noreply.github.com>
2023-06-16 16:19:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,365 @@
+const quizData = {
+    "questions": [
+      {
+        "question": "Which planet is known as the 'Red Planet'?",
+        "answers": [
+          {"text": "Mars", "isCorrect": true},
+          {"text": "Jupiter", "isCorrect": false},
+          {"text": "Saturn", "isCorrect": false},
+          {"text": "Venus", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Who painted the Mona Lisa?",
+        "answers": [
+          {"text": "Pablo Picasso", "isCorrect": false},
+          {"text": "Leonardo da Vinci", "isCorrect": true},
+          {"text": "Vincent van Gogh", "isCorrect": false},
+          {"text": "Michelangelo", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "What is the capital city of Australia?",
+        "answers": [
+          {"text": "Melbourne", "isCorrect": false},
+          {"text": "Sydney", "isCorrect": false},
+          {"text": "Canberra", "isCorrect": true},
+          {"text": "Brisbane", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Which famous physicist developed the theory of relativity?",
+        "answers": [
+          {"text": "Isaac Newton", "isCorrect": false},
+          {"text": "Stephen Hawking", "isCorrect": false},
+          {"text": "Galileo Galilei", "isCorrect": false},
+          {"text": "Albert Einstein", "isCorrect": true},
+        ]
+      },
+      {
+        "question": "Which country is famous for the Taj Mahal?",
+        "answers": [
+          {"text": "India", "isCorrect": true},
+          {"text": "China", "isCorrect": false},
+          {"text": "Egypt", "isCorrect": false},
+          {"text": "Italy", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "What is the largest planet in our solar system?",
+        "answers": [
+          {"text": "Saturn", "isCorrect": false},
+          {"text": "Jupiter", "isCorrect": true},
+          {"text": "Neptune", "isCorrect": false},
+          {"text": "Uranus", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Who wrote the famous play 'Romeo and Juliet'?",
+        "answers": [
+          {"text": "Jane Austen", "isCorrect": false},
+          {"text": "Mark Twain", "isCorrect": false},
+          {"text": "William Shakespeare", "isCorrect": true},
+          {"text": "Charles Dickens", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "What is the capital city of France?",
+        "answers": [
+          {"text": "Rome", "isCorrect": false},
+          {"text": "Berlin", "isCorrect": false},
+          {"text": "London", "isCorrect": false},
+          {"text": "Paris", "isCorrect": true},
+        ]
+      },
+      {
+        "question": "Who painted the famous artwork 'The Starry Night'?",
+        "answers": [
+          {"text": "Vincent van Gogh", "isCorrect": true},
+          {"text": "Salvador Dalí", "isCorrect": false},
+          {"text": "Pablo Picasso", "isCorrect": false},
+          {"text": "Leonardo da Vinci", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Which country is known as the 'Land of the Rising Sun'?",
+        "answers": [
+          {"text": "China", "isCorrect": false},
+          {"text": "Japan", "isCorrect": true},
+          {"text": "Thailand", "isCorrect": false},
+          {"text": "South Korea", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Who invented the telephone?",
+        "answers": [
+          {"text": "Thomas Edison", "isCorrect": false},
+          {"text": "Nikola Tesla", "isCorrect": false},
+          {"text": "Alexander Graham Bell", "isCorrect": true},
+          {"text": "Guglielmo Marconi", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Which city hosted the 2016 Summer Olympics?",
+        "answers": [
+          {"text": "London", "isCorrect": false},
+          {"text": "Beijing", "isCorrect": false},
+          {"text": "Tokyo", "isCorrect": false},
+          {"text": "Rio de Janeiro", "isCorrect": true},
+        ]
+      },
+      {
+        "question": "What is the chemical symbol for gold?",
+        "answers": [
+          {"text": "Au", "isCorrect": true},
+          {"text": "Ag", "isCorrect": false},
+          {"text": "Cu", "isCorrect": false},
+          {"text": "Pt", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "Which famous scientist formulated the theory of evolution?",
+        "answers": [
+          {"text": "Albert Einstein", "isCorrect": false},
+          {"text": "Charles Darwin", "isCorrect": true},
+          {"text": "Isaac Newton", "isCorrect": false},
+          {"text": "Marie Curie", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "What is the tallest mountain in the world?",
+        "answers": [
+          {"text": "K2", "isCorrect": false},
+          {"text": "Kangchenjunga", "isCorrect": false},
+          {"text": "Mount Everest", "isCorrect": true},
+          {"text": "Makalu", "isCorrect": false},
+        ]
+      },
+      {
+        "question": "What is the largest organ in the human body?",
+        "answers": [
+          {"text": "Liver", "isCorrect": false},
+          {"text": "Heart", "isCorrect": false},
+          {"text": "Lungs", "isCorrect": false},
+          {"text": "Skin", "isCorrect": true}
+        ]
+      },
+      {
+        "question": "Which country is home to the Great Barrier Reef?",
+        "answers": [
+          {"text": "Australia", "isCorrect": true},
+          {"text": "Brazil", "isCorrect": false},
+          {"text": "Mexico", "isCorrect": false},
+          {"text": "Canada", "isCorrect": false}
+        ]
+      },
+      {
+        "question": "Who is the author of the Harry Potter book series?",
+        "answers": [
+          {"text": "Stephen King", "isCorrect": false},
+          {"text": "J.K. Rowling", "isCorrect": true},
+          {"text": "George R.R. Martin", "isCorrect": false},
+          {"text": "Agatha Christie", "isCorrect": false}
+        ]
+      },
+      {
+        "question": "Which country is famous for the Pyramids of Giza?",
+        "answers": [
+          {"text": "Greece", "isCorrect": false},
+          {"text": "Peru", "isCorrect": false},
+          {"text": "Egypt", "isCorrect": true},
+          {"text": "Turkey", "isCorrect": false}
+        ]
+      },
+      {
+        "question": "Who painted the famous artwork 'The Last Supper'?",
+        "answers": [
+          {"text": "Vincent van Gogh", "isCorrect": false},
+          {"text": "Pablo Picasso", "isCorrect": false},
+          {"text": "Salvador Dalí", "isCorrect": false},
+          {"text": "Leonardo da Vinci", "isCorrect": true},
+        ]
+      },
+      {
+        "question": "Which animal is known as the 'King of the Jungle'?",
+        "answers": [
+          {"text": "Lion", "isCorrect": true},
+          {"text": "Elephant", "isCorrect": false},
+          {"text": "Tiger", "isCorrect": false},
+          {"text": "Giraffe", "isCorrect": false}
+        ]
+      },
+      {
+        "question": "What is the chemical symbol for water?",
+        "answers": [
+          {"text": "CO2", "isCorrect": false},
+          {"text": "H2O", "isCorrect": true},
+          {"text": "NaCl", "isCorrect": false},
+          {"text": "C6H12O6", "isCorrect": false}
+        ]
+      },
+      {
+        "question": "Which city is famous for the Colosseum?",
+        "answers": [
+          {"text": "Athens", "isCorrect": false},
+          {"text": "Istanbul", "isCorrect": false},
+          {"text": "Rome", "isCorrect": true},
+          {"text": "Paris", "isCorrect": false},
+        ]
+      }
+      // Add more questions...
+    ]
+
+}
+
+// NUMBER OF QUESTIONS
+let x = 15
+  
+// Accessing the data
+
+/*quizData.questions.forEach((question, index) => {
+console.log(`Question ${index + 1}: ${question.question}`);
+question.answers.forEach((answer, answerIndex) => {
+    console.log(`Answer ${answerIndex + 1}: ${answer.text}`);
+    console.log(`Is Correct: ${answer.isCorrect}`);
+});
+});*/
+
+// CONSTANTS
+const FrontPage = document.querySelector('.FrontPage')
+const QuizPage = document.querySelector('.QuizPage')
+const ResPage = document.querySelector('.ResPage')
+
+document.querySelector('.FP_btn').onclick = function(){
+  FrontPage.classList.add('hide')
+  QuizPage.classList.remove('hide')
+}
+
+// function ResPage() {
+//   QuizPage.classList.add('hide')
+//   ResPage.classList.remove('hide')
+// }
+
+// function
+
+function randomizeTheme(){
+  let n = Math.floor(Math.random() * 3)
+  console.log(n)
+  // arr of classes
+  const ansBtnStClasses = ['btn_y','btn_g','btn_b']
+  const ansBgStClasses = ['bg_y','bg_g','bg_b']
+
+  // selecting (.QP_aBtn) & (.QuizPage)
+  const ansBtnsSt = document.getElementsByClassName('QP_aBtn')
+  const ansBgSt = document.querySelector('.QuizPage')
+
+  // remove all 3 classes from (.QuizPage)
+  for (let i =0; i < 3; i ++){
+    ansBgSt.classList.remove(ansBgStClasses[i])
+  }
+
+  // add class[n] to (.QuizPage)
+  ansBgSt.classList.add(ansBgStClasses[n])
+
+
+  // remove all 3 classes from (.QP_aBtn)
+  for (let i =0; i < 3; i ++){
+    for(let btn = 0 ; btn < 4; btn++){
+      ansBtnsSt[btn].classList.remove(ansBtnStClasses[i])
+    }
+  }
+
+  // add class[n] to (.QP_aBtn)
+  for(let btn = 0 ; btn < 4; btn++){
+    ansBtnsSt[btn].classList.add(ansBtnStClasses[n])
+  }
+}
+
+
+
+function checkRes(pick,i){
+  console.log(quizData.questions[i])
+  if (quizData.questions[i].answers[pick].isCorrect) {
+      console.log('correct ' + pick)
+      cor++
+  }
+  else {
+      console.log('incorrect ' + pick)
+      // console.log('correct answer: ' + findCorrectAnswer(i))
+  }
+}
+
+function Res() {
+  console.log(cor + "/"+all +" correct")
+  updateScore()
+  QuizPage.classList.add('hide')
+  ResPage.classList.remove('hide')
+}
+
+
+function quiz(n){
+  randomizeTheme()
+  let rand =  Math.floor(Math.random()*questionsArr.length)
+  let i = questionsArr[rand]
+  questionsArr.splice(rand, 1);
+
+  let numOfQ = quizData.questions.length
+  const q = document.querySelector('.QP_q')
+  // change question text
+  q.textContent = quizData.questions[i].question
+  const ansBtns = document.querySelectorAll('.QP_aBtn')
+
+  // change answer text
+  ansBtns[0].textContent = quizData.questions[i].answers[0].text
+  ansBtns[1].textContent = quizData.questions[i].answers[1].text
+  ansBtns[2].textContent = quizData.questions[i].answers[2].text
+  ansBtns[3].textContent = quizData.questions[i].answers[3].text
+
+  n++
+
+  ansBtns[0].onclick = function(){
+      checkRes(0,i)
+      if (n < x) quiz(n)
+      else Res()
+  }
+  ansBtns[1].onclick = function(){
+      checkRes(1,i)
+      if (n < x) quiz(n)
+      else Res()
+  }
+  ansBtns[2].onclick = function(){
+      checkRes(2,i)
+      if (n < x) quiz(n)
+      else Res()
+  }
+  ansBtns[3].onclick = function(){
+      checkRes(3,i)
+      if (n < x) quiz(n)
+      else Res()
+  }
+  all++
+}
+
+// execution
+let all = 0
+let cor = 0
+let questionsArr = Array.from(Array(quizData.questions.length).keys())
+let rand = 0
+let cur = 0
+
+  quiz(0)
+
+
+
+const resScore = document.querySelector('.RP_scoreText')
+const resScoreLabel = document.querySelector('.RP_scoreLabel')
+
+function updateScore(){
+  resScore.textContent = Math.round(100*cor/all) + "%"
+  console.log(cor)
+  console.log(all)
+  resScoreLabel.textContent = cor+'/'+all+' Correct Answers'
+}
+
+const restartQuizBtn = document.querySelector('.RP_retakeBtn')
+restartQuizBtn.onclick = () => location.reload()
